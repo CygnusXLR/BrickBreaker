6af1e759bfc2e08aa33756a268d7c11acd7d0536
CygnusXLR <55222340+CygnusXLR@users.noreply.github.com>
2020-11-20 15:26:14 -0800

    Add files via upload

---
+++ BrickBreaker.cpp
@@ -0,0 +1,18 @@
+/*
+ * BrickBreaker.cpp
+ *
+ *  Created on: Jun 14, 2020
+ *      Author: rbill637
+ */
+
+#include "BrickBreaker.h"
+
+BrickBreaker::BrickBreaker() {
+	// TODO Auto-generated constructor stub
+
+}
+
+BrickBreaker::~BrickBreaker() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ BrickBreaker.h
@@ -0,0 +1,17 @@
+/*
+ * BrickBreaker.h
+ *
+ *  Created on: Jun 14, 2020
+ *      Author: rbill637
+ */
+
+#ifndef BRICKBREAKER_H_
+#define BRICKBREAKER_H_
+
+class BrickBreaker {
+public:
+	BrickBreaker();
+	virtual ~BrickBreaker();
+};
+
+#endif /* BRICKBREAKER_H_ */
